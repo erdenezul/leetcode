2a94c4977d7126d0b24daafdbda47df4bffb8ef2
Erdenezul Batmunkh <erdenezul.b@gmail.com>
2020-07-02 21:30:57 +0200

    Add min ways to make coin change

---
+++ min_coin_change.py
@@ -0,0 +1,25 @@
+def min_coin_change(amount, coins):
+    """
+    Returns minimum number of coins to make amount
+    n -> number of coins
+    m -> amount
+    Runtime Complexity: O(n * m)
+    Auxiliary Space: O(m)
+
+    >>> min_coin_change(51, [1, 15, 10])
+    5
+    """
+    coins.sort()
+    dp = [amount + 1 for _ in range(amount + 1)]
+    dp[0] = 0
+    for i in range(amount + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], 1 + dp[i - coin])
+            else:
+                break
+    return dp[amount] if dp[amount] < amount else -1
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
